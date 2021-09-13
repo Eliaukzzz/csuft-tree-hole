cae45de51cebe43917cfc0a416d6e5599fbea2be
Eliauk <2421398452@qq.com>
2021-09-13 22:51:59 +0800

    前端静态页面实现

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 林科大树洞
+## tree-hole-react 为前端
+## tree-hole-C++ 为后端
---
+++ tree-hole-react/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      minHeight: {
+        "main-h": "96vh",
+      },
+      minWidth: {
+        "main-w": "94vw",
+      },
+      spacing: {
+        "main-p": "2vh",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
