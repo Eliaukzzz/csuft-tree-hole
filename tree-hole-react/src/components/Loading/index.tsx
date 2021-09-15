42830d79f3acc989c5b079563f811cf014ffe5b8
Eliauk <2421398452@qq.com>
2021-09-16 03:15:21 +0800

    修复Loading的样式bug

--- tree-hole-react/src/components/Loading/index.tsx
+++ tree-hole-react/src/components/Loading/index.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 
 export const Loading = () => {
   return (
-    <div className="flex flex-col justify-center items-center fixed top-0 left-0 bottom-0 right-0 bg-gary-theme-gary bg-opacity-80">
+    <div className="m-4 flex z-50 flex-col justify-center items-center fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-80">
       <h2 className="text-white text-3xl">Loading......</h2>
       <div className="spinner"></div>
     </div>
