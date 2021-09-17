eb547446e84576af3f2e30bb889363fd69815423
Eliauk <2421398452@qq.com>
2021-09-18 00:10:03 +0800

    完成UserContext的登录，登出和获取用户信息功能

--- tree-hole-FE/src/context/index.tsx
+++ tree-hole-FE/src/context/index.tsx
@@ -2,13 +2,16 @@ import React, { ReactNode } from "react";
 import { CommentListProvider } from "./CommentsListContext";
 import { LoadProvider } from "./Load";
 import { ShowNavProvider } from "./ShowNav";
+import { UserContextProvider } from "./UserContext";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
     <LoadProvider>
-      <CommentListProvider>
-        <ShowNavProvider>{children}</ShowNavProvider>
-      </CommentListProvider>
+      <ShowNavProvider>
+        <UserContextProvider>
+          <CommentListProvider>{children}</CommentListProvider>
+        </UserContextProvider>
+      </ShowNavProvider>
     </LoadProvider>
   );
 };
