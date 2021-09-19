febf5dabbe18d5d3be564fd895bc15e76cd0cef3
Eliauk <2421398452@qq.com>
2021-09-19 17:50:55 +0800

    更新mock数据

--- tree-hole-FE/JsonServerMock/middleware.js
+++ tree-hole-FE/JsonServerMock/middleware.js
@@ -12,12 +12,14 @@ module.exports = (req, res, next) => {
           gender: "男",
           password: "123456",
           avatar: "https://z3.ax1x.com/2021/09/05/hReKSA.jpg",
+          favorite: [1, 2],
+          hate: [3, 4],
           token:
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjI0MjEzOTg0NTJAcXEuY29tIiwicGFzc3dvcmQiOiIxMjM0NTYifQ.WOqAshEJnXX7clWA8HU0EAhOHCfVqSL15SbYHGYIycA",
         },
       });
     } else {
-      return res.status(400).json({ message: "用户名或者密码错误" });
+      return res.status(400).json({ message: "邮箱或者密码错误" });
     }
   }
   next();
